d6aaa84ab01610cc454a7a4e20c73ca0fd1dcfee
Anessa Ortner <anessa.ortner@gmail.com>
2017-04-17 20:30:34 -0500

    working write to dom functions and promise structures

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,46 @@
-console.log("working js file");
-$(document).ready(function(){
+var categoryTypeName = [];
+$(".dropdown-menu").click(function(e){
+	$("#products").html("");
 	var storeArray = [];
-
+	var clickTarget =  e.target.id;
+	
+	function writeToDOM(){
+		var categoryId = "";
+		var categoryName = "";
+		var categoryTypeId = [];
+		
+		var idCounter = 0;
+		
+		
+			for (let x = 0; x < storeArray.length; x++){
+				if (storeArray[x].value === "categories" && storeArray[x].name === clickTarget){
+					categoryId = storeArray[x].id;
+					categoryName = storeArray[x].name;
+				}
+				if (storeArray[x].value === "types" && categoryId === storeArray[x].category) {
+					var typeId = storeArray[x].id;
+					categoryTypeId.push(typeId);
+					categoryTypeName.push(storeArray[x].name);
+					console.log(categoryTypeName);
+				}
+			}
+			for (let y = 0; y < storeArray.length; y++) {
+				var newType = categoryTypeName[storeArray[y].type];
+				if (storeArray[y].value === "products" && storeArray[y].type === categoryTypeId[idCounter]) {
+					if (y % 3 === 1) {
+						idCounter++;
+					}
+					
+					$("#products").append(`<div class="productCard col-md-4">
+											<h3>${storeArray[y].name}</h3>
+											<p>${storeArray[y].description}</p>
+											<p id="category">Category: ${categoryName}</p>
+											<p class="type">${newType}</p>
+											</div>`);
+				}
+			}		
+	}
+	
 	var loadCategoriesJSON = function(){
 		return new Promise(function(resolve, reject){
 			$.ajax("./db/categories.json").done(function(data1){
@@ -31,20 +70,29 @@ $(document).ready(function(){
 	};
 
 	loadCategoriesJSON().then(function(categoriesData){
-		storeArray = categoriesData;
-		console.log("first request", storeArray);
+		categoriesData.forEach(function(data){
+		storeArray.push(data);
+		data.value = "categories";
+		});
 		return loadTypesJSON();
 	}).then(function(typesData){
 		typesData.forEach(function(types){
 			storeArray.push(types);
+			types.value = "types";
 		});
-		console.log("second request", storeArray);
 		return loadProductsJSON();
 	}).then(function(productsData){
+
 		productsData.forEach(function(products){
-			storeArray.push(products);
+			for (var key in products) {
+				if(products.hasOwnProperty(key)) {
+					storeArray.push(products[key]);
+					products[key].value = "products";
+				}
+			}
 		});
-		console.log("final array", storeArray);
+		writeToDOM();
+
 	});
 
 
